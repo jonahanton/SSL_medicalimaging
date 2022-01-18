b23573527d4e5fddb5cc2d824fe5b8568668cdc4
jonahanton <jonahlouisanton@gmail.com>
2022-01-18 14:36:23 +0000

    Adding methods to simclr

--- models/base.py
+++ models/base.py
@@ -7,3 +7,16 @@ class BaseModel(pl.LightningModule):
 
     def __init__(self):
         super().__init__()
+
+    
+    def forward(self, X):
+        pass
+
+
+    def training_step(self, batch):
+        pass
+
+    
+    def validation_step(self, batch):
+        pass
+
--- models/simclr.py
+++ models/simclr.py
@@ -9,3 +9,19 @@ class SimCLR(BaseModel):
 
     def __init__(self):
         super().__init__()
+
+    
+    def forward(self, X):
+        return super().forward(X)
+
+    
+    def training_step(self, batch):
+        return super().training_step(batch)
+
+
+    def validation_step(self, batch):
+        return super().validation_step(batch)
+
+    
+    def simclr_loss(self):
+        pass
