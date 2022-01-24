ded1b07d4efd3f8e78dc4162512b5e944f30449b
jonahanton <jonahlouisanton@gmail.com>
2022-01-24 20:58:33 +0000

    Merge conflict

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from methods.simclr import SimCLRTrainer
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-data-path', default='./datasets', help='path to dataset')
-parser.add_argument('-dataset-name', default='MNIST', help='dataset name')
+parser.add_argument('--data-path', default='./datasets', help='path to dataset')
+parser.add_argument('--dataset-name', default='MNIST', help='dataset name')
 parser.add_argument('--epochs', default=100, help='total number of epochs to train for')
 parser.add_argument('--batch-size', default=256)
 parser.add_argument('--lr', default=1e-3)
@@ -20,7 +20,8 @@ parser.add_argument('--weight-decay', default=1e-6)
 parser.add_argument('--output-dim', default=10)
 parser.add_argument('--temperature', default=0.5)
 parser.add_argument('--n-views', default=2)
-# To do: parse in argument for architecture
+parser.add_argument('--arch', default='simple')
+parser.add_argument('--outpath', default='saved_models')
 
 def main():
 
@@ -36,7 +37,7 @@ def main():
         train_dataset = datasets.MNIST(args.data_path, train=True, transform=data_transforms, download=True)
         train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
     
-    model = SimCLRBase(output_dim=args.output_dim)
+    model = SimCLRBase(arch=args.arch, output_dim=args.output_dim)
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=len(train_loader), eta_min=0, last_epoch=-1)
 
