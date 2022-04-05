120a402ea533aae1ed45733f1cdab8779703fa19
jonahanton <jonahlouisanton@gmail.com>
2022-04-05 18:37:26 +0100

    Updated slurm scripts

--- bash_scripts/slurm_submits/slurm_fewshot.sh
+++ bash_scripts/slurm_submits/slurm_fewshot.sh
@@ -7,9 +7,9 @@
 export PATH=/vol/bitbucket/${USER}/sslvenv/bin/:$PATH
 source activate
 source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
-TERM=vt100  # TERM=xterm
+# TERM=vt100  # TERM=xterm
 /usr/bin/nvidia-smi
-uptime
+# uptime
 
 cd /vol/bitbucket/jla21/SEGP/SSL
 python few_shot.py -d cifar10 -m moco-v2 --n-way 5 --n-support 20
--- bash_scripts/slurm_submits/slurm_linear.sh
+++ bash_scripts/slurm_submits/slurm_linear.sh
@@ -7,9 +7,9 @@
 export PATH=/vol/bitbucket/${USER}/sslvenv/bin/:$PATH
 source activate
 source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
-TERM=vt100  # TERM=xterm
+# TERM=vt100  # TERM=xterm
 /usr/bin/nvidia-smi
-uptime
+# uptime
 
 cd /vol/bitbucket/jla21/SEGP/SSL
 python linear.py -d cifar10 -m moco-v2 -c 316227.7712565657
