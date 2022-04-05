a4b3167f5fda343fea62023de138d188fc1e85c6
Jonah Anton <jla21@vm-test-slurm.doc.ic.ac.uk>
2022-04-05 16:39:54 +0100

    Bash scripts to submit linear to slurm

--- bash_scripts/slurm_submits/slurm_linear.sh
+++ bash_scripts/slurm_submits/slurm_linear.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 #SBATCH --gres=gpu:1
-#SBATCH –-mail-type=ALL
-#SBATCH –-mail-user=jla21
+#SBATCH --output=out/linear_%j.out
+
 
 export PATH=/vol/bitbucket/${USER}/sslvenv/bin/:$PATH
 source activate
 source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
 TERM=vt100  # TERM=xterm
-
-echo starting script
-cd /vol/bitbucket/jla21/SEGP/SSL
-python linear.py -d cifar10 -m moco-v2 -c 316227.7712565657
-
 /usr/bin/nvidia-smi
 uptime
+
+cd /vol/bitbucket/jla21/SEGP/SSL
+python linear.py -d cifar10 -m moco-v2 -c 316227.7712565657
 
