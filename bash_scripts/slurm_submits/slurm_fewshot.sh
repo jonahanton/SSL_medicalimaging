1994e2f713443bb9eb964d7d35c9448947c40a15
Jonah Anton <jla21@cloud-vm-40-251.doc.ic.ac.uk>
2022-04-07 10:29:25 +0100

    Edits to slurm submits

--- bash_scripts/slurm_submits/slurm_fewshot.sh
+++ bash_scripts/slurm_submits/slurm_fewshot.sh
@@ -4,13 +4,13 @@
 #SBATCH --output=out/fewshot_%j.out
 
 
-export PATH=/vol/bitbucket/${USER}/sslvenv/bin/:$PATH
+export PATH=/vol/bitbucket/g21mscprj03/sslvenv/bin/:$PATH
 source activate
 source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
 # TERM=vt100  # TERM=xterm
 /usr/bin/nvidia-smi
 # uptime
 
-cd /vol/bitbucket/jla21/SEGP/SSL
+cd /vol/bitbucket/g21mscprj03/SSL
 python few_shot.py -d cifar10 -m moco-v2 --n-way 5 --n-support 20
 
--- bash_scripts/slurm_submits/slurm_linear.sh
+++ bash_scripts/slurm_submits/slurm_linear.sh
@@ -4,13 +4,13 @@
 #SBATCH --output=out/linear_%j.out
 
 
-export PATH=/vol/bitbucket/${USER}/sslvenv/bin/:$PATH
+export PATH=/vol/bitbucket/g21mscprj03/sslvenv/bin/:$PATH
 source activate
 source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
 # TERM=vt100  # TERM=xterm
 /usr/bin/nvidia-smi
 # uptime
 
-cd /vol/bitbucket/jla21/SEGP/SSL
+cd /vol/bitbucket/g21mscprj03/SSL
 python linear.py -d cifar10 -m moco-v2 -c 316227.7712565657
 
