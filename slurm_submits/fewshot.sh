4b1b0e575ed776f639de0adf79b7f5e1d84a4827
Jonah Anton <jla21@vm-test-slurm.doc.ic.ac.uk>
2022-04-07 12:39:15 +0100

    Fiddled with slurm submit scripts

--- slurm_submits/fewshot.sh
+++ slurm_submits/fewshot.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+dset=cifar100
+model=moco-v2
+
 #SBATCH --gres=gpu:1
-#SBATCH --output=/vol/bitbucket/g21mscprj03/SSL/out/fewshot_%j.out
+#SBATCH --output=/vol/bitbucket/g21mscprj03/SSL/out/few-shot/$dset_$model_%j.out
 
 
 export PATH=/vol/bitbucket/g21mscprj03/sslvenv/bin/:$PATH
 source activate
 source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
-# TERM=vt100  # TERM=xterm
+TERM=vt100  # TERM=xterm
 /usr/bin/nvidia-smi
-# uptime
+uptime
 
 cd /vol/bitbucket/g21mscprj03/SSL
-python few_shot.py -d cifar10 -m moco-v2 --n-way 5 --n-support 20
 
+python few_shot.py -d $dset -m $model --n-way 5 --n-support 20
--- slurm_submits/linear.sh
+++ slurm_submits/linear.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-dset=shenzhencxr
+dset=cifar10
+model=moco-v2
 
 #SBATCH --gres=gpu:1
-#SBATCH -output=out/linear_%j.out
+#SBATCH --output=/vol/bitbucket/g21mscprj03/SSL/out/linear/$dset_$model_%j.out
 
 export PATH=/vol/bitbucket/g21mscprj03/sslvenv/bin/:$PATH
 source activate
 source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
-# TERM=vt100  # TERM=xterm
+TERM=vt100  # TERM=xterm
 /usr/bin/nvidia-smi
-# uptime
+uptime
 
 cd /vol/bitbucket/g21mscprj03/SSL
+python linear.py -d $dset -m $model
 
-# python linear.py -d cifar10 -m moco-v2 -c 316227.7712565657
-python linear.py -d $dset -m moco-v2
