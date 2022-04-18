f0f935b0b7849ada18c3882c1164090fee9540d5
unknown <you@example.com>
2022-04-19 00:58:18 +0100

    Added mha file conversion code for stoic

--- reconstruction/convert_utils.py
+++ reconstruction/convert_utils.py
@@ -93,7 +93,57 @@ def create_white_image():
     out.save(filepath, "JPEG")
     print(f"Succesfully created white patch.")
 
+def convert_from_mha():
+    models = ["simclr-v1"]
+
+    filepath = '/Users/Wan Hee/Downloads/reconstructed_images2/reconstructed_images2'
+
+    mha_images = []
+
+    for model in models:
+        for filename in glob.glob(str(filepath + model + "/*mha")):
+            im = Image.open(filename)
+            temp_filename = filename.split(".")[0:-1]
+            temp_filename = ".".join(temp_filename)
+
+            out_filename = str(temp_filename) + ".jpeg"
+            
+            print(out_filename)
+
+            try:
+                out = im.convert("RGB")
+                out.save(out_filename, "JPEG")
+                print(f"Succesfully converted input image {filename} to output image {out_filename}.")
+
+            except:
+                print(f"WARNING! Did not convert input image {filename}")
+                print(f"Output file is probably wrong {out_filename}")
+
+def convert_from_mha_and_resize():
+    filepath = '/Users/Wan Hee/Downloads/reconstructed_images2/reconstructed_images2/sample_images/stoic'
+
+    for filename in glob.glob(str(filepath +  "/*mha")):
+        im = Image.open(filename)
+        temp_filename = filename.split(".")[0:-1]
+        temp_filename = ".".join(temp_filename)
+
+        out_filename = str(temp_filename) + ".jpeg"
+
+        print(out_filename)
+
+        # try:
+        out = im.convert("RGB")
+
+        transform = transforms.Compose([
+                transforms.Resize(224, interpolation=PIL.Image.BICUBIC),
+                transforms.CenterCrop(224)
+            ])
+        out = transform(out)
+        out.save(out_filename, "JPEG")
+        print(f"Succesfully converted input image {filename} to output image {out_filename}.")
+
 
 if __name__ == "__main__":
-    convert_from_tif()
+    # convert_from_tif()
     # create_white_image()
+    convert_from_mha()
