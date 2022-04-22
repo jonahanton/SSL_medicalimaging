6547f935d62b042cb300faa7679742d7b91a9ee4
Mun Fai <mc821@ic.ac.uk>
2022-04-22 21:02:09 +0100

    replaced reconstruction script with new script

--- slurm_submits/reconstruction.sh
+++ slurm_submits/reconstruction.sh
@@ -11,19 +11,34 @@ uptime
 
 cd /vol/bitbucket/g21mscprj03/SSL
 
-input_dir='./sample_images/diabetic_retinopathy/34680_left.jpeg'
-max_iter=1000
+max_iter=3000
 clip=True
+dset=imagenet
 
-python reconstruction.py -m swav --input_dir $input_dir --max_iter $max_iter --clip $clip
-python reconstruction.py -m byol --input_dir $input_dir --max_iter $max_iter --clip $clip
-python reconstruction.py -m pirl --input_dir $input_dir --max_iter $max_iter --clip $clip
-python reconstruction.py -m moco-v2 --input_dir $input_dir --max_iter $max_iter --clip $clip
-python reconstruction.py -m mimic-chexpert_lr_0.01 --input_dir $input_dir --max_iter $max_iter --clip $clip
-python reconstruction.py -m mimic-chexpert_lr_0.1 --input_dir $input_dir --max_iter $max_iter --clip $clip
-python reconstruction.py -m mimic-chexpert_lr_1.0 --input_dir $input_dir --max_iter $max_iter --clip $clip
-python reconstruction.py -m mimic-cxr_r18_lr_1e-4 --input_dir $input_dir --max_iter $max_iter --clip $clip
-python reconstruction.py -m mimic-cxr_d121_lr_1e-4 --input_dir $input_dir --max_iter $max_iter --clip $clip
-python reconstruction.py -m supervised_r50 --input_dir $input_dir --max_iter $max_iter --clip $clip
-python reconstruction.py -m supervised_r18 --input_dir $input_dir --max_iter $max_iter --clip $clip
-python reconstruction.py -m supervised_d121 --input_dir $input_dir --max_iter $max_iter --clip $clip
+# python reconstruction.py -m simclr-v1 -d bach chestx chexpert diabetic_retinopathy ichallenge_amd ichallenge_pm montgomerycxr shenzhencxr stoic imagenet --max_iter $max_iter --clip $clip
+# python reconstruction.py -m swav -d bach chestx chexpert diabetic_retinopathy ichallenge_amd ichallenge_pm montgomerycxr shenzhencxr --max_iter $max_iter --clip $clip
+# python reconstruction.py -m byol -d bach chestx chexpert diabetic_retinopathy ichallenge_amd ichallenge_pm montgomerycxr shenzhencxr --max_iter $max_iter --clip $clip
+# python reconstruction.py -m pirl -d bach chestx chexpert diabetic_retinopathy ichallenge_amd ichallenge_pm montgomerycxr shenzhencxr --max_iter $max_iter --clip $clip
+# python reconstruction.py -m moco-v2 -d bach chestx chexpert diabetic_retinopathy ichallenge_amd ichallenge_pm montgomerycxr shenzhencxr --max_iter $max_iter --clip $clip
+# python reconstruction.py -m mimic-chexpert_lr_0.01 -d bach chestx chexpert diabetic_retinopathy ichallenge_amd ichallenge_pm montgomerycxr shenzhencxr --max_iter $max_iter --clip $clip
+# python reconstruction.py -m mimic-chexpert_lr_0.1 -d bach chestx chexpert diabetic_retinopathy ichallenge_amd ichallenge_pm montgomerycxr shenzhencxr --max_iter $max_iter --clip $clip
+# python reconstruction.py -m mimic-chexpert_lr_1.0 -d bach chestx chexpert diabetic_retinopathy ichallenge_amd ichallenge_pm montgomerycxr shenzhencxr --max_iter $max_iter --clip $clip
+# python reconstruction.py -m mimic-cxr_r18_lr_1e-4 -d bach chestx chexpert diabetic_retinopathy ichallenge_amd ichallenge_pm montgomerycxr shenzhencxr --max_iter $max_iter --clip $clip
+# python reconstruction.py -m mimic-cxr_d121_lr_1e-4 -d bach chestx chexpert diabetic_retinopathy ichallenge_amd ichallenge_pm montgomerycxr shenzhencxr --max_iter $max_iter --clip $clip
+# python reconstruction.py -m supervised_r50 -d bach chestx chexpert diabetic_retinopathy ichallenge_amd ichallenge_pm montgomerycxr shenzhencxr --max_iter $max_iter --clip $clip
+# python reconstruction.py -m supervised_r18 -d bach chestx chexpert diabetic_retinopathy ichallenge_amd ichallenge_pm montgomerycxr shenzhencxr --max_iter $max_iter --clip $clip
+# python reconstruction.py -m supervised_d121 -d bach chestx chexpert diabetic_retinopathy ichallenge_amd ichallenge_pm montgomerycxr shenzhencxr --max_iter $max_iter --clip $clip
+
+python reconstruction.py -m simclr-v1 -d $dset --max_iter $max_iter --clip $clip
+python reconstruction.py -m swav -d $dset --max_iter $max_iter --clip $clip
+python reconstruction.py -m byol -d $dset --max_iter $max_iter --clip $clip
+python reconstruction.py -m pirl -d $dset --max_iter $max_iter --clip $clip
+python reconstruction.py -m moco-v2 -d $dset --max_iter $max_iter --clip $clip
+python reconstruction.py -m mimic-chexpert_lr_0.01 -d $dset --max_iter $max_iter --clip $clip
+python reconstruction.py -m mimic-chexpert_lr_0.1 -d $dset --max_iter $max_iter --clip $clip
+python reconstruction.py -m mimic-chexpert_lr_1.0 -d $dset --max_iter $max_iter --clip $clip
+python reconstruction.py -m mimic-cxr_r18_lr_1e-4 -d $dset --max_iter $max_iter --clip $clip
+python reconstruction.py -m mimic-cxr_d121_lr_1e-4 -d $dset --max_iter $max_iter --clip $clip
+python reconstruction.py -m supervised_r50 -d $dset --max_iter $max_iter --clip $clip
+python reconstruction.py -m supervised_r18 -d $dset --max_iter $max_iter --clip $clip
+python reconstruction.py -m supervised_d121 -d $dset --max_iter $max_iter --clip $clip
