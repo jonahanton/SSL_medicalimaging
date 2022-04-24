072c8377721446a11df74b9caa9a2d1fd00615ee
mc821@ic.ac.uk <mc821@ic.ac.uk>
2022-04-24 22:43:17 +0100

    Moved backbones code outside of few shot

---
+++ models/backbones.py
@@ -0,0 +1,92 @@
+import os
+import logging
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+
+class ResNet18Backbone(nn.Module):
+    def __init__(self, model_name):
+        super().__init__()
+        self.model_name = model_name
+
+        self.model = models.resnet18(pretrained=False)
+        del self.model.fc
+
+        state_dict = torch.load(os.path.join('models', self.model_name + '.pth'))
+        self.model.load_state_dict(state_dict)
+
+        self.model.train()
+        print("Number of model parameters:", sum(p.numel() for p in self.model.parameters()))
+        logging.info("Number of model parameters:", sum(p.numel() for p in self.model.parameters()))
+
+    def forward(self, x):
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
+
+        x = self.model.layer1(x)
+        x = self.model.layer2(x)
+        x = self.model.layer3(x)
+        x = self.model.layer4(x)
+
+        x = self.model.avgpool(x)
+        x = torch.flatten(x, 1)
+
+        return x
+
+
+class ResNetBackbone(nn.Module):
+    def __init__(self, model_name):
+        super().__init__()
+        self.model_name = model_name
+
+        self.model = models.resnet50(pretrained=False)
+        del self.model.fc
+
+        state_dict = torch.load(os.path.join('models', self.model_name + '.pth'))
+        self.model.load_state_dict(state_dict)
+
+        self.model.train()
+        print("num parameters:", sum(p.numel() for p in self.model.parameters()))
+        logging.info("Number of model parameters:", sum(p.numel() for p in self.model.parameters()))
+
+    def forward(self, x):
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
+
+        x = self.model.layer1(x)
+        x = self.model.layer2(x)
+        x = self.model.layer3(x)
+        x = self.model.layer4(x)
+
+        x = self.model.avgpool(x)
+        x = torch.flatten(x, 1)
+
+        return x
+
+
+class DenseNetBackbone(nn.Module):
+    def __init__(self, model_name):
+        super().__init__()
+        self.model_name = model_name
+
+        self.model = models.densenet121(pretrained=False)
+        del self.model.classifier
+
+        state_dict = torch.load(os.path.join('models', self.model_name + '.pth'))
+        self.model.load_state_dict(state_dict)
+
+        self.model.train()
+        print("Number of model parameters:", sum(p.numel() for p in self.model.parameters()))
+        logging.info("Number of model parameters:", sum(p.numel() for p in self.model.parameters()))
+
+    def forward(self, x):
+        features = self.model.features(x)
+        out = F.relu(features, inplace=True)
+        out = F.adaptive_avg_pool2d(out, (1, 1))
+        out = torch.flatten(out, 1)
+        return out
